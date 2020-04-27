c8ead226750b9330a97f756621849b95fe9eaeea
Alnezis <alnezis@ya.ru>
2020-04-27 19:55:29 +0300

    new field error

--- vk/object/ResponseError.go
+++ vk/object/ResponseError.go
@@ -1,7 +1,9 @@
 package object
 
 type ResponseError struct {
-	ErrorCode float64        `json:"error_code"`
-	Error     string         `json:"error_msg"`
-	Params    []RequestParam `json:"request_params"`
+	ErrorCode  float64        `json:"error_code"`
+	Error      string         `json:"error_msg"`
+	Params     []RequestParam `json:"request_params"`
+	CaptchaSid string         `json:"captcha_sid,omitempty"`
+	CaptchaImg string         `json:"captcha_img,omitempty"`
 }
